6a71ff739b121cb858b221d2aed48d87d5c1b29c
SimonAllenOliver <simonallenoliver@gmail.com>
2024-01-13 15:56:48 -0600

    update python

--- server.py
+++ server.py
@@ -1,14 +1,20 @@
-from flask import Flask, render_template  # added render_template!
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
-
-@app.route('/')
 def index():
-    return render_template("index.html", phrase="hello", times=5)
+    return render_template("index.html")
 
-# app.run(debug=True) should be the very last statement! 
+
+@app.route('/index2/<times>/')
+def index2(times):
+    return render_template("index2.html", times = int(times))	# notice the 2 new named arguments!
+
+@app.route('/index2/<times>/<color>')
+def index2(times):
+    return render_template("index3.html", times = int(times), color = "color")	# notice the 2 new named arguments!
+
+
 if __name__=="__main__":
     app.run(debug=True)
+
